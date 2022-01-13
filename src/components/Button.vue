b37c3b1c4edd5211f990d1a69dbb57cf738df7c1
Tareq Kmail <TKmail@asaltech.com>
2022-01-13 10:31:18 +0200

    first-commit

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button @click="$emit('show-add-task')" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:{
+        text:String,
+        color:String,
+    }
+    
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @show-add-task="$emit('toggle-add-task')" :text="showaddtask ? 'Close' : 'AddTask'" :color="showaddtask ? 'red' : 'green'"/>
+    </header>
+</template>
+
+
+<script>
+import Button from './Button'
+export default {
+    name:'Header',
+    props:{
+        title:{
+            type:String,
+            default:"Tareq",
+            
+        },
+        showaddtask:Boolean,
+    },
+        components:{
+            Button,
+        },
+        
+        
+    
+
+}
+</script>
+
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
